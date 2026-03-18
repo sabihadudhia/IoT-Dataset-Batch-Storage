0f9841437c0d2f3d11964bd76193ecd514fa898d
Sabiha <sabiha.dudhia@iu-study.org>
2026-03-18 15:48:58 +0200

    Add unique index on sensor_readings collection

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 db = db.getSiblingDB('sensor_db');
 db.createCollection('sensor_readings');
 db.sensor_readings.createIndex({ ts: 1 })
+db.sensor_readings.createIndex({ device: 1, ts: 1 }, { unique: true })
 
 db.createCollection('sensor_rejected');
 db.sensor_rejected.createIndex({ ts: 1 })
