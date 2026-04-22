07cb27f71cc1288525aa2174851e10821d3003be
Sabiha <sabiha.dudhia@iu-study.org>
2026-04-23 00:27:45 +0200

    Add sensor_alerts collection and index

--- init.js
+++ init.js
@@ -6,3 +6,7 @@ db.sensor_readings.createIndex({ device: 1, ts: 1 }, { unique: true })
 db.createCollection('sensor_rejected');
 db.sensor_rejected.createIndex({ ts: 1 })
 db.sensor_rejected.createIndex({ device: 1 })
+
+db.createCollection('sensor_alerts');
+db.sensor_alerts.createIndex({ device: 1, field: 1, active: 1 })
+
